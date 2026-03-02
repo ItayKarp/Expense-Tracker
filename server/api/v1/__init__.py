e96381f79a6b1549713dc19e78de11395da2cdc2
Badonke <itaykarp@gmail.com>
2026-03-02 08:49:27 +0200

    updated readme

--- server/api/v1/__init__.py
+++ server/api/v1/__init__.py
@@ -1,3 +1,3 @@
 from .authenticate_endpoints import authentication_router
-from .dashboard_endpoints import dashboard_router
+from .expenses_endpoints import dashboard_router
 from .statistics_endpoints import statistics_router
